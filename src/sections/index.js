6ec6078cde2b518f3f76db733ad889e3c3868510
SajjanKarn <karnaa787@gmail.com>
2023-09-15 20:36:36 +0545

    hero section

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Popular from "./Popular";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  Footer,
+  CustomerReviews,
+  Popular,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
